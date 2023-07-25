3ae725ef6fc62f593d8029fa2855c616f9c8dc12
nessakodo <nessakodo@gmail.com>
2023-07-25 10:33:50 -0600

    adding multiway decision making with if-elif in FahrCel.py

--- Review/FahrCel.py
+++ Review/FahrCel.py
@@ -16,8 +16,18 @@ def calc_celsius(fahrenheit):
     return round((fahrenheit - 32) * (5/9), 2)
 
 def output_data(celsius):
-    # Modify this function as per your desired output or processing
-    print("The temperature is", celsius, "degrees Celsius.")
+    if celsius < -20:
+        print("The temperature is very cold. Bundle up!")
+    elif -20 <= celsius < 0:
+        print("The temperature is quite cold.")
+    elif 0 <= celsius < 15:
+        print("The temperature is cool.")
+    elif 15 <= celsius < 25:
+        print("The temperature is moderate.")
+    elif 25 <= celsius < 30:
+        print("The temperature is warm. Don't forget to stay hydrated!")
+    else:
+        print("The temperature is hot. Sunscreen is necessary!")
 
 def main():
     try:
